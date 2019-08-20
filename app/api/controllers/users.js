fb150970cb07379c484707cd1cdb333813e6f181
redemptive <ewan_forbes@yahoo.co.uk>
2019-08-20 19:38:33 +0100

    Small change to directly call validationResult from express validator

--- app/api/controllers/users.js
+++ app/api/controllers/users.js
@@ -10,12 +10,11 @@ const ServerError = require('../config/ServerError');
 
 module.exports = {
 	create: function(req, res, next) {
-		let user = new User(req.body);
-		let firstUser = false;
+		// If there are no validation errors
+		if (validationResult(req).isEmpty()) {
+			let user = new User(req.body);
+			let firstUser = false;
 
-		const errors = validationResult(req);
-
-		if (errors.isEmpty()) {
 			User.findOne({}, (err, user) => {
 				// If this is the first user we make them an admin
 				if (!user) firstUser = true;
